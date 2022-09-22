7c9b7893341627f81b7e1acd9b4e78f9e6e0556a
Artem Chetverikov <artemchetverikov@gmail.com>
2022-09-22 00:09:15 -0700

    Add chip stash

---
+++ PotsAndPotions.Core.Tests/Pot/StashTests.cs
@@ -0,0 +1,104 @@
+﻿using NuGet.Frameworks;
+using PotsAndPotions.Core.Chips;
+using PotsAndPotions.Core.Color;
+using PotsAndPotions.Core.Pot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotsAndPotions.Core.Tests.Pot
+{
+    public class StashTests
+    {
+        [Fact]
+        public void AddChip_NoChips_HasAChipInAllChips()
+        {
+            var stash = new Stash();
+
+            Assert.Empty(stash.AllChips);
+
+            stash.AddChip(new Chip(1, Colors.Orange, 1));
+
+            Assert.Single(stash.AllChips);
+        }
+
+        [Fact]
+        public void DrawSingle_NoChipsRemaing_OneChipRemaining()
+        {
+            var stash = new Stash();
+
+            stash.AddChip(new Chip(1, Colors.Orange, 1));
+
+            stash.DrawSingle();            
+
+            Assert.Empty(stash.RemainingChips);
+
+            stash.AddChip(new Chip(1, Colors.Orange, 1));
+
+            Assert.Single(stash.RemainingChips);
+        }
+
+        [Fact]
+        public void Reset_OneChipDrawn_CanDrawAgain()
+        {
+            var stash = new Stash();
+
+            stash.AddChip(new Chip(1, Colors.Orange, 1));
+
+            stash.DrawSingle();
+
+            Assert.Empty(stash.RemainingChips);
+
+            stash.Reset();
+
+            Assert.Single(stash.RemainingChips);
+        }
+
+        [Fact]
+        public void DrawSingle_TwoChipsRemaining_OneChipRemaining()
+        {
+            var stash = new Stash();
+
+            stash.AddChip(new Chip(1, Colors.Orange, 1));
+            stash.AddChip(new Chip(1, Colors.Orange, 1));
+
+            Assert.Equal(2, stash.RemainingChips.Count);
+
+            stash.DrawSingle();
+
+            Assert.Single(stash.RemainingChips);
+        }
+
+        [Fact]
+        public void DrawSingle_NoChipsRemaining_NoChipReturned()
+        {
+            var stash = new Stash();
+
+            Assert.Empty(stash.RemainingChips);
+
+            var drawnChip = stash.DrawSingle();
+
+            Assert.Null(drawnChip);
+        }
+
+        [Fact]
+        public void ReturnDrawn_NoChipsRemaining_1ChipRemaining()
+        {
+            var stash = new Stash();
+
+            stash.AddChip(new Chip(1, Colors.Orange, 1));
+
+            var drawnChip = stash.DrawSingle();
+
+            Assert.NotNull(drawnChip);
+            Assert.Empty(stash.RemainingChips);
+
+            stash.ReturnDrawn(drawnChip);
+
+            Assert.Single(stash.RemainingChips);
+            Assert.Single(stash.AllChips);
+        }
+    }
+}
---
+++ PotsAndPotions.Core/Pot/Stash.cs
@@ -0,0 +1,47 @@
+﻿using PotsAndPotions.Core.Chips;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotsAndPotions.Core.Pot
+{
+    public class Stash
+    {
+        private Random random = new Random();
+
+        public List<IChip> AllChips { get; } = new List<IChip>();
+
+        public List<IChip> RemainingChips { get; private set; } = new List<IChip>();
+        
+        public void Reset()
+        {
+            RemainingChips = AllChips.ToList();
+        }
+
+        public IChip? DrawSingle()
+        {
+            if(RemainingChips.Count == 0)
+            {
+                return null;
+            }
+
+            var chipIndex = random.Next(0, RemainingChips.Count - 1);
+            var chip = RemainingChips[chipIndex];
+            RemainingChips.RemoveAt(chipIndex);
+            return chip;
+        }
+
+        public void ReturnDrawn(IChip chip)
+        {
+            RemainingChips.Add(chip);
+        }
+
+        public void AddChip(IChip chip)
+        {
+            AllChips.Add(chip);
+            RemainingChips.Add(chip);
+        }
+    }
+}
